632d0fc15219af140b9f0a0eaf3fdc290f8f9fc3
Ehsan <me.hosaini@gmail.com>
2017-10-12 21:54:04 -0400

    reorganized code in an OO manner

--- js/mvvm.js
+++ js/mvvm.js
@@ -1,19 +1,71 @@
+// maps center on page load
+var mapCenter = {
+      center: {lat: 38.8896198, lng: -77.0229772},
+      zoom: 13
+  };
+
+// google maps object constructors
+var googleMapObject = {
+  makeMap: function(){
+    // Create a map object and specify the DOM element for display.
+    var map = new google.maps.Map(document.getElementById("map"), mapCenter);
+    return map;
+  },
+
+  makeMaker: function(site,map) {
+    var marker = new google.maps.Marker({
+      position: site,
+      map: map,
+    });
+    return marker;
+  },
+
+  makeInfoWindow: function(content){
+    var infoWindow = new google.maps.InfoWindow({
+      content: content
+    })
+    return infoWindow;
+  }
+};
+
+
 // site constructor function
 function Site(site){
-  this.name = site.name;
-  this.location = site.location;
-};
+    this.name = site.name;
+    this.location = site.location;
+  };
 
-
-function ViewModel(){
-  var self = this;
-  self.sites = ko.observableArray([])
-
+// knockout view model object
+var ViewModel = {
   // populate the sites observableArray upon page load
-  defaultLocations.forEach(function(site){
-    self.sites.push(new Site(site));
-  });//-- end of forEach
+  sites: ko.observableArray([]),
+
+  populateSites: function(){
+    var theViewModelObj = this;
+    defaultLocations.forEach(function(site){
+      theViewModelObj.sites.push(new Site(site));
+    });//-- end of forEach
+  },
+  zoomOnSite: function(){
+    console.log(this);
+  }
 
 };
 
-ko.applyBindings(new ViewModel());
+//initiate Knockout on page load
+ko.applyBindings(ViewModel);
+ViewModel.populateSites();
+
+// callback for googlemaps api
+function initMap(){
+  // Create a map object and specify the DOM element for display.
+  var map = googleMapObject.makeMap();
+  // Create a marker for each location
+  ViewModel.sites().forEach(function(site){
+     var infoWindow = googleMapObject.makeInfoWindow(site.name);
+     var marker = googleMapObject.makeMaker(site.location, map);
+     marker.addListener('click', function(){
+       infoWindow.open(map, marker);
+      });
+    });//-- end of forEach
+  };
--- js/script.js
+++ js/script.js
@@ -1,21 +1,5 @@
-// var defaultLocations = [
-//   {name: "Smithsonian National Air and Space Museum" , lat: 38.88816010000001, lng: -77.01986789999999 },
-//   {name: "George Washington University", lat: 38.8997145, lng: 77.0485992 },
-//   {name: "Dupont Circle" , lat: 38.9096936, lng: -77.043339 },
-//   {name: "The White House" , lat: 38.8976763, lng: -77.0365298 },
-//   {name: "The Pentagon" , lat: 38.8718568, lng: -77.0562669 }
-// ];
 
-// var defaultLocationsxx = [
-//   {lat: 38.896945, lng: -77.0236171},
-//   {lat: 38.9076089, lng: -77.07225849999999},
-//   {lat: 38.8997145, lng: 77.0485992},
-//   {lat: 38.9096936, lng: -77.043339},
-//   {lat: 38.8976763, lng: -77.0365298},
-//   {lat: 38.8718568, lng: -77.0562669}
-// ];
-
-
+// locations data
 var defaultLocations = [
   { name:"Washington National Cathedral",
     location: {lat: 38.9305946, lng: -77.0707807}
@@ -35,38 +19,4 @@ var defaultLocations = [
   { name:"The Pentagon",
     location: {lat: 38.8718568, lng: -77.0562669}
   },
-]
-
-function initMap(){
-  // Create a map object and specify the DOM element for display.
-  var map = new google.maps.Map(document.getElementById("map"),{
-    center: {lat: 38.8896198, lng: -77.0229772},
-    zoom: 13
-  }); //-- end of map variable
-
-
-  // mark defaultLocations on the map
-  defaultLocations.forEach(function(site){
-    var infoWindow = new google.maps.InfoWindow({
-      content: site.name
-    });//-- end of infoWindow
-    var marker = makeMaker(site, map);
-    marker.addListener('click', function(){
-        infoWindow.open(map, marker);
-    }
-  );//-- end of addEventListener
-
-  }); //-- end of defaultLocations.forEach
-
-}; //-- end of initMap()
-
-
-// create markers
-function makeMaker(site, map){
-  var marker = new google.maps.Marker({
-    position: site.location,
-    map: map,
-    // title: site.name
-  });
-  return marker;
-};//-- end of makeMaker
+];
