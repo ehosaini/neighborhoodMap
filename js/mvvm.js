3f6ed0be91b8bf86673003f6a8dcaa4ec55072da
Ehsan <me.hosaini@gmail.com>
2017-10-14 17:13:22 -0400

    make code object oriented

--- js/mvvm.js
+++ js/mvvm.js
@@ -1,32 +1,10 @@
+
+
 // maps center on page load
 var mapCenter = {
       center: {lat: 38.8896198, lng: -77.0229772},
       zoom: 13
   };
-
-// google maps object constructors
-var googleMapObject = {
-  makeMap: function(){
-    // Create a map object and specify the DOM element for display.
-    var map = new google.maps.Map(document.getElementById("map"), mapCenter);
-    return map;
-  },
-
-  makeMaker: function(site,map) {
-    var marker = new google.maps.Marker({
-      position: site,
-      map: map,
-    });
-    return marker;
-  },
-
-  makeInfoWindow: function(content){
-    var infoWindow = new google.maps.InfoWindow({
-      content: content
-    })
-    return infoWindow;
-  }
-};
 
 
 // site constructor function
@@ -35,37 +13,96 @@ function Site(site){
     this.location = site.location;
   };
 
-// knockout view model object
-var ViewModel = {
+// Knockout view model object
+function ViewModel(){
   // populate the sites observableArray upon page load
-  sites: ko.observableArray([]),
+    self = this;
+    self.sites = ko.observableArray([]),
 
-  populateSites: function(){
-    var theViewModelObj = this;
-    defaultLocations.forEach(function(site){
-      theViewModelObj.sites.push(new Site(site));
-    });//-- end of forEach
-  },
-  zoomOnSite: function(){
-    console.log(this);
-  }
+    self.populateSites = function(){
+      // var theViewModelObj = this;
+      defaultLocations.forEach(function(site){
+        self.sites.push(new Site(site));
+      });//-- end of forEach
+    };
+
+    // self.map = initMap();
+
+    self.zoomOnSite = function(site){
+      // self.map.setCenter(site.location);
+      console.log(site.name);
+    };
 
 };
 
-//initiate Knockout on page load
-ko.applyBindings(ViewModel);
-ViewModel.populateSites();
 
-// callback for googlemaps api
+var initializer = {
+  init: function(object){
+    // set the mapsObject property
+    this.mapsObject= object;
+  },
+
+  mapMaker: function(){
+    // Create and render map
+    var map = this.mapsObject.makeMap();
+    return map;
+  },
+  // Create a marker for each location given data from
+  // Knockout observableArray data
+  makeMakers: function(knockout){
+    // Create a marker for each location
+    var initializerObject = this;
+    map = this.mapMaker();
+    knockout.sites().forEach(function(site){
+       var infoWindow = initializerObject.mapsObject.makeInfoWindow(site.name);
+       var marker = initializerObject.mapsObject.makeMaker(site.location, map);
+       // open info window when user clicks on a marker
+       marker.addListener('click', function(){
+         infoWindow.open(map, marker);
+        });
+    })//-- end of forEach function
+  }//-- end of markerMaker property
+};
+
+// make a Knockout view model object
+var controller = new ViewModel()
+
+// populate the sites observableArray
+controller.populateSites();
+
+//initiate Knockout on page load
+ko.applyBindings(controller);
+
+
+// callback for googlemaps JS api
 function initMap(){
-  // Create a map object and specify the DOM element for display.
-  var map = googleMapObject.makeMap();
-  // Create a marker for each location
-  ViewModel.sites().forEach(function(site){
-     var infoWindow = googleMapObject.makeInfoWindow(site.name);
-     var marker = googleMapObject.makeMaker(site.location, map);
-     marker.addListener('click', function(){
-       infoWindow.open(map, marker);
+
+  // google maps object constructors
+  var googleMapsObject = {
+    makeMap: function(){
+      // Create a map object and specify the DOM element for display.
+      var map = new google.maps.Map(document.getElementById("map"), mapCenter);
+      return map;
+    },
+
+    makeMaker: function(site,map) {
+      var marker = new google.maps.Marker({
+        position: site,
+        map: map,
       });
-    });//-- end of forEach
+      return marker;
+    },
+
+    makeInfoWindow: function(content){
+      var infoWindow = new google.maps.InfoWindow({
+        content: content
+      })
+      return infoWindow;
+    }
   };
+
+
+  //draw maps and markers
+  initializer.init(googleMapsObject);
+  initializer.makeMakers(controller);
+};
