f0b2d557ffb3f6c882b40e9b9ca332e64b6de08a
Ehsan <me.hosaini@gmail.com>
2017-10-15 18:10:32 -0400

    infowindow opens when user filters or clicks on a name

--- js/mvvm.js
+++ js/mvvm.js
@@ -2,18 +2,20 @@
 // maps center on page load
 var mapCenter = {
       center: {lat: 38.8718568, lng: -77.0562669},
-      zoom: 13
+      zoom: 12
   };
 
 
 // site constructor function
-function Site(site){
+function Site(site, marker, infoWindow){
     this.name = site.name;
     this.location = site.location;
+    this.siteMarker = marker;
+    this.siteInfoWindow = infoWindow;
   };
 
 // Knockout view model object
-function ViewModel(map){
+function ViewModel(map, marker, infoWindow){
   // populate the sites observableArray upon page load
     self = this;
     self.sites = ko.observableArray([]),
@@ -21,14 +23,20 @@ function ViewModel(map){
     self.populateSites = function(){
       // var theViewModelObj = this;
       defaultLocations.forEach(function(site){
-        self.sites.push(new Site(site));
+        self.sites.push(new Site(site, marker, infoWindow));
       });//-- end of forEach
     };
 
-  // center the map on the location
-  // that user click on it's name on the list
+  // center the map on the marker of location that user click on it's name
+  //on the list and open the infowindow
     self.zoomOnSite = function(site){
       map.setCenter(site.location);
+      map.setZoom(16);
+      var siteMarker = site.siteMarker(site.location, map);
+      var siteInfoWindow = site.siteInfoWindow(site.name);
+      // siteInfoWindow.setPosition(site.location);
+      siteInfoWindow.open(map, siteMarker);
+      // console.log(siteInfoWindow);
     };
 };
 
@@ -48,9 +56,9 @@ var initializer = {
   // Create a marker for each location given data from
   // Knockout observableArray data
   makeMakers: function(knockout, map){
-    // Create a marker for each location
+
     var initializerObject = this;
-    // map = this.mapMaker();
+    // Create a marker for each location in Konckout observableArray
     knockout.sites().forEach(function(site){
        var infoWindow = initializerObject.mapsObject.makeInfoWindow(site.name);
        var marker = initializerObject.mapsObject.makeMaker(site.location, map);
@@ -82,22 +90,52 @@ function initMap(){
       return marker;
     },
 
+    returnMarker: function(){
+      function returnMarker(site, map){
+        var marker = new google.maps.Marker({
+          position: site,
+          map: map,
+        });
+        return marker;
+      };//-- end of inner function
+      return returnMarker;
+    },
+
+    // returned value is used in eventlistner when the map renders on page laod
     makeInfoWindow: function(content){
       var infoWindow = new google.maps.InfoWindow({
         content: content
-      })
+      });
       return infoWindow;
+    },
+
+    // returned value used in Site constructor which stores the value
+    // in the Knockout observableArray
+    returnInfoWindow: function(){
+      function returnInfoWindow(content){
+        var infoWindow = new google.maps.InfoWindow({
+          content: content
+        });//-- end of inner function
+        return infoWindow;
+      };
+
+      return returnInfoWindow;
     }
   };
 
 
   //draw maps and markers
   initializer.init(googleMapsObject);
-  var map = initializer.mapMaker();
 
-//--------------------
+  // objects passed into Knockout viewmodel
+  var map = initializer.mapMaker();
+  var marker =  googleMapsObject.returnMarker();
+  var infoWindow = googleMapsObject.returnInfoWindow();
+
+
+//-------------------- initilize and bind Knokout view model --------------
   // make a Knockout view model object
-  var controller = new ViewModel(map);
+  var controller = new ViewModel(map, marker, infoWindow);
 
   // populate the sites observableArray
   controller.populateSites();
@@ -106,9 +144,6 @@ function initMap(){
   ko.applyBindings(controller);
 //--------------------
 
-  // zoomPoint.push(map);
+  // load initial map and set markers
   initializer.makeMakers(controller, map);
-
 };
-
-// console.log(zoomPoint);
