647fbfbd446284633a3ae98970d582526c72a6db
Ehsan <me.hosaini@gmail.com>
2017-10-25 21:15:43 -0400

    moved non-knockout code into script.js

--- js/data.js
+++ js/data.js
@@ -1,28 +1,50 @@
-
 // locations data
-var defaultLocations = [
-  { name:"Dolcezza Gelato",
+var defaultLocations = [{
+    name: "Dolcezza Gelato",
     category: "coffee shop",
-    location: {lat: 38.91419768029149, lng: -77.04425211970849}
+    location: {
+      lat: 38.91419768029149,
+      lng: -77.04425211970849
+    }
   },
-  { name:"Georgetown University",
-    category: "educationl institute",
-    location: {lat: 38.9076089, lng: -77.072258}
+  {
+    name: "Georgetown University",
+    category: "educational institute",
+    location: {
+      lat: 38.9076089,
+      lng: -77.072258
+    }
   },
-  { name:"George Washington University",
-    category: "educationl institute",
-    location: {lat: 38.8997145, lng: -77.0485992}
+  {
+    name: "George Washington University",
+    category: "educational institute",
+    location: {
+      lat: 38.8997145,
+      lng: -77.0485992
+    }
   },
-  { name:"Dupont Circle",
+  {
+    name: "Dupont Circle",
     category: "park",
-    location: {lat: 38.9096936, lng: -77.043339}
+    location: {
+      lat: 38.9096936,
+      lng: -77.043339
+    }
   },
-  { name:"Museum of African American History and Culture",
+  {
+    name: "Museum of African American History and Culture",
     category: "muesuem",
-    location: {lat: 38.8910545, lng: -77.03270359999999}
+    location: {
+      lat: 38.8910545,
+      lng: -77.03270359999999
+    }
   },
-  { name:"Barcode",
+  {
+    name: "Barcode",
     category: "restaurant",
-    location: {lat: 38.9039407, lng: -77.0380103}
+    location: {
+      lat: 38.9039407,
+      lng: -77.0380103
+    }
   },
 ];
--- js/script.js
+++ js/script.js
@@ -21,3 +21,170 @@ defaultLocations.forEach(function(loc) {
 function googleError() {
   $("#map").html("Sorry we couldn't load the map, try again!")
 }
+
+// maps center on page load
+var mapCenter = {
+  center: {
+    lat: 38.9096936,
+    lng: -77.043339
+  },
+  zoom: 12
+};
+
+// site constructor function that populates Konckout observableArray
+function Site(site) {
+  this.name = site.name;
+  this.location = site.location;
+  this.category = site.category;
+  this.ajaxUrl = site.ajaxUrl;
+  this.siteMarker = "";
+  this.siteInfoWindow = "";
+  this.visible = ko.observable(true);
+}
+
+// initializer object properties of which are used in
+// initMap() call back
+var initializer = {
+  init: function(object) {
+    // set the mapsObject property
+    this.mapsObject = object;
+  },
+
+  mapMaker: function() {
+    // Create and render map
+    var map = this.mapsObject.makeMap();
+    return map;
+  },
+  // Create a marker for each location given data from
+  // Knockout observableArray data
+  makeMakers: function(knockout, map) {
+    var initializerObject = this;
+    // Create a marker for each location in Konckout observableArray
+    knockout.sites().forEach(function(site) {
+      var infoWindow = initializerObject.mapsObject.makeInfoWindow(site.name);
+      var marker = initializerObject.mapsObject.makeMaker(site.location, map);
+      site.siteMarker = marker;
+      site.siteInfoWindow = infoWindow;
+      // animate marker and open info window when user clicks on a marker
+      // and make ajax call to fourSquare api
+      marker.addListener('click', function() {
+        // animate marker when clicked on
+        if (marker.getAnimation() !== null) {
+          marker.setAnimation(null);
+        } else {
+          marker.setAnimation(google.maps.Animation.BOUNCE);
+        }
+        // open infoWindow
+        infoWindow.open(map, marker);
+
+        // make Ajax call to foursquare api
+        $.ajax({
+          url: site.ajaxUrl,
+          method: 'GET',
+          dataType: "json",
+        }).done(function(result) {
+          var data = result.response.venues;
+          foursquareData = {};
+          foursquareData.venueName = data[0].name;
+          foursquareData.venueAddress = data[0].location.formattedAddress;
+          foursquareData.category = data[0].categories[0].name;
+          foursquareData.url = data[0].url;
+
+          var address = foursquareData.venueAddress;
+          $("#venueName").html(foursquareData.venueName);
+          $("#venueAddress").html(address[0] + ", " + address[1], +address[2]);
+
+          if (foursquareData.url !== undefined) {
+            $("#url").css("display", "block");
+            $("#url").html("Website");
+            $("#url").attr({
+              href: foursquareData.url,
+              target: "_blank"
+            });
+          } // end of if
+
+        }).fail(function(err) {
+          var errorMessage = "Sorry foursquare couldn't find a match :(";
+          $("#venueName").html(errorMessage);
+        });
+      }); //-- end of addListener
+    }); //-- end of forEach function
+  } //-- end of markerMaker property
+};
+
+
+//------------------------ callback for googlemaps JS api ---------------------
+function initMap() {
+
+  // google maps objects constructor
+  var googleMapsObject = {
+    makeMap: function() {
+      // Create a map object and specify the DOM element for display.
+      var map = new google.maps.Map(document.getElementById("map"), mapCenter);
+      return map;
+    },
+
+    makeMaker: function(site, map) {
+      var marker = new google.maps.Marker({
+        position: site,
+        map: map
+      });
+      return marker;
+    },
+
+    // returned value used in Site constructor which stores the value
+    // in the Knockout observableArray
+    returnMarker: function() {
+      function returnMarker(site, map) {
+        var marker = new google.maps.Marker({
+          position: site,
+          map: map,
+        });
+        return marker;
+      } //-- end of inner function
+      return returnMarker;
+    },
+
+    // returned value is used in eventlistner when the map renders on page laod
+    makeInfoWindow: function(content) {
+      var infoWindow = new google.maps.InfoWindow({
+        content: content
+      });
+      return infoWindow;
+    },
+
+    // returned value used in Site constructor which stores the value
+    // in the Knockout observableArray
+    returnInfoWindow: function() {
+      function returnInfoWindow(content) {
+        var infoWindow = new google.maps.InfoWindow({
+          content: content
+        }); //-- end of inner function
+        return infoWindow;
+      }
+
+      return returnInfoWindow;
+    }
+  };
+
+  //draw maps and markers
+  initializer.init(googleMapsObject);
+
+  // objects passed into Knockout viewmodel
+  var map = initializer.mapMaker();
+
+
+  //-------------------- initilize and bind Knockout view model on page load ----
+  // make a Knockout view model object
+  var controller = new ViewModel(map);
+
+  // populate the sites observableArray
+  controller.populateSites();
+
+  //initiate Knockout on page load
+  ko.applyBindings(controller);
+  //-----------------------------------------------------------------------------
+
+  // load initial map and set markers
+  initializer.makeMakers(controller, map);
+} //-- end of initMap callback
